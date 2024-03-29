516704455532a7e516660e93a2231f602bd19674
nahid <nahidrabbi045@gmail.com>
2024-03-29 22:08:46 +0600

    home section update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import BooksDetails from "./BooksDetails";
+
+
+const Home = () => {
+  return (
+    <div>
+ 
+      <Hero></Hero>
+      <BooksDetails></BooksDetails>
+    </div>
+  );
+};
+
+export default Home;
